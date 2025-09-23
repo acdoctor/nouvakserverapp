8ef72781fa006ebcab87ce008ca6f0d77e584a69
sachin sharma <sachin.wiz91@gmail.com>
2025-09-23 12:45:17 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`server running on http://localhost: ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running on http://localhost: ${PORT}`),
+);
 
 // adding a comment to test husky and lint-staged
+// adding another comment to test husky and lint-staged
