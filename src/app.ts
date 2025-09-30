aec53a3bb618096754241b270dea958201891115
sachin sharma <sachinacdoctor@gmail.com>
2025-09-30 17:35:43 +0530

    chore: connect mongoDB atlas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
+// import
+
 dotenv.config();
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+// app.use("/api/v1", adminRoutes);
 
 export default app;
