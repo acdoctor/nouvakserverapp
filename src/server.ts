36512c60d24a24c29e55a6c4284186bd03d4cf1d
sachin sharma <sachinacdoctor@gmail.com>
2025-09-30 16:17:25 +0530

    chore: Added TypeScript configuration (tsconfig.json) with CommonJS for Node.js

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import express from "express";
-
-const app = express();
-const PORT = 3000;
+import app from "./app";
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
 app.listen(PORT, () =>
-  console.log(`server running on http://localhost: ${PORT}`),
+  console.log(`server running on http://localhost:${PORT}`),
 );
