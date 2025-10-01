27c7e0bb2fa373df0d1f0ae391cd3a1d6f3d3134
sachin sharma <sachinacdoctor@gmail.com>
2025-10-01 10:29:36 +0530

    add: added admin and otp schema

---
+++ src/models/admin/admin.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: Number,
+    },
+    role: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
