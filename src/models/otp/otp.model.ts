e15e9308b7c4571910e7a9627cdce66ebfcd5963
sachin sharma <sachinacdoctor@gmail.com>
2025-10-03 16:35:12 +0530

    chore: increase token expire time.

--- src/models/otp/otp.model.ts
+++ src/models/otp/otp.model.ts
@@ -9,7 +9,7 @@ export interface IOtp extends Document {
 const OtpSchema: Schema = new Schema({
   adminId: { type: Schema.Types.ObjectId, ref: "Admin", required: true },
   otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 300 }, // auto-delete after 5 min
+  createdAt: { type: Date, default: Date.now, expires: 432000 }, // auto-delete after 5 days
 });
 
 export default mongoose.model<IOtp>("Otp", OtpSchema);
