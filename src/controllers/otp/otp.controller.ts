06079c69275fab15fd6f98b93905f1d6b14a2b84
sachin sharma <sachinacdoctor@gmail.com>
2025-10-03 15:58:53 +0530

    feat(otp): issue access and refresh tokens after verification

--- src/controllers/otp/otp.controller.ts
+++ src/controllers/otp/otp.controller.ts
@@ -1,6 +1,7 @@
 import * as otpService from "../../services/otp/otp.service";
 import Admin from "../../models/admin/admin.model";
 import { Request, Response } from "express";
+import { generateAccessToken, generateRefreshToken } from "../../utils/jwt";
 
 export const resendOtp = async (req: Request, res: Response) => {
   try {
@@ -22,7 +23,29 @@ export const verifyOtp = async (req: Request, res: Response) => {
     const { userId, otp } = req.body;
     await otpService.verifyOtp(userId, otp);
 
-    res.json({ message: "OTP verified, user activated" });
+    const admin = await Admin.findById(userId);
+    if (!admin) return res.status(404).json({ message: "Admin not found" });
+
+    //  Generate tokens
+    const accessToken = generateAccessToken({
+      id: admin._id,
+      role: admin.role,
+    });
+    const refreshToken = generateRefreshToken({
+      id: admin._id,
+      role: admin.role,
+    });
+
+    //  Save refreshToken in DB
+    admin.refreshToken = refreshToken;
+    admin.status = "active";
+    await admin.save();
+
+    res.json({
+      message: "OTP verified, admin activated",
+      accessToken,
+      refreshToken,
+    });
   } catch (err: unknown) {
     res
       .status(400)
