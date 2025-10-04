53d3e2451ff8c9cdbb581fcc6414a061221f0edd
sachin sharma <sachinacdoctor@gmail.com>
2025-10-04 17:47:01 +0530

    chore: add admin profile route to check access token

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import * as adminController from "../../controllers/admin/admin.controller";
+import { authenticate } from "../../middlewares/auth";
 
 const router = Router();
 
 router.post("/admin/register", adminController.register);
 router.post("/admin/login", adminController.login);
+router.post("/admin/profile", authenticate);
 
 export default router;
