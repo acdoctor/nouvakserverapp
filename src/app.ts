ad273f9ada2bc0da6fec06876a05f4f21ba88dd6
sachin sharma <sachinacdoctor@gmail.com>
2025-10-06 18:45:48 +0530

    chore: add cookie parser to get refresh token in cookie request

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
+import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/admin/admin.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 connectDB();
 app.use("/api/v1", adminRoutes);
