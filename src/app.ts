65e70aca7cce868b32fe002e871b0a6398847f22
sachin sharma <sachinacdoctor@gmail.com>
2025-10-06 17:17:58 +0530

    refactor: move refresh access token logic to admin folder to separate it for admin

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
 import adminRoutes from "./routes/admin/admin.routes";
-import authRoutes from "./routes/auth/auth.routes";
 
 const app = express();
 
@@ -11,6 +10,5 @@ app.use(express.json());
 
 connectDB();
 app.use("/api/v1", adminRoutes);
-app.use("/api/v1", authRoutes);
 
 export default app;
