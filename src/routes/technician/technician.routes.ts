973fefe65cba44fbfef153aecfd11d00314022be
sachin sharma <sachinacdoctor@gmail.com>
2025-10-09 16:05:17 +0530

    feat: create routes for technician

---
+++ src/routes/technician/technician.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+// import * as adminController from "../../controllers/admin/admin.controller";
+import * as otpController from "../../controllers/technician/otp.controller";
+import * as authController from "../../controllers/technician/auth.controller";
+import * as technicianController from "../../controllers/technician/technician.controller";
+import { authenticate } from "../../middlewares/technician/auth";
+
+const router = Router();
+
+// Technician Controllers Routes
+router.post("/technician/register", technicianController.registerTechnician);
+router.post("/technician/login", technicianController.login);
+router.post("/technician/resend-otp", otpController.resendOtp);
+router.post("/technician/verify-otp", otpController.verifyOtp);
+router.post("/technician/refresh", authController.refresh);
+router.post("/technician/profile", authenticate, (req, res) => {
+  res.json({ message: "Technician profile accessed" });
+});
+
+// Technician Controllers Routes
+router.post(
+  "/admin/technician/register",
+  technicianController.registerTechnician,
+);
+export default router;
