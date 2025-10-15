fb500a62cad9a160faad445ef7fde8842f134cd2
sachin sharma <sachinacdoctor@gmail.com>
2025-10-15 17:02:53 +0530

    feat: add auth validator to technician routes

--- src/routes/technician/technician.routes.ts
+++ src/routes/technician/technician.routes.ts
@@ -3,12 +3,22 @@ import * as otpController from "../../controllers/technician/otp.controller";
 import * as authController from "../../controllers/technician/auth.controller";
 import * as technicianController from "../../controllers/technician/technician.controller";
 import { authenticate } from "../../middlewares/technician/auth";
+import { authSchema } from "../../validators/request/auth.validator";
+import { validateRequest } from "../../middlewares/request/validateRequest";
 
 const router = Router();
 
 // Technician Controllers Routes
-router.post("/technician/register", technicianController.registerTechnician);
-router.post("/technician/login", technicianController.loginTechnician);
+router.post(
+  "/technician/register",
+  validateRequest(authSchema),
+  technicianController.registerTechnician,
+);
+router.post(
+  "/technician/login",
+  validateRequest(authSchema),
+  technicianController.loginTechnician,
+);
 router.post("/technician/resend-otp", otpController.resendOtp);
 router.post("/technician/verify-otp", otpController.verifyOtp);
 router.post("/technician/refresh", authController.refresh);
