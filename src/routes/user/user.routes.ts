693ab43c2759ddae2252cd11efb9e32db1f4301a
sachin sharma <sachinacdoctor@gmail.com>
2025-10-15 17:29:45 +0530

    feat: add auth validator to user routes

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -3,12 +3,22 @@ import * as userController from "../../controllers/user/user.controller";
 import * as otpController from "../../controllers/user/otp.controller";
 import * as authController from "../../controllers/user/auth.controller";
 import { authenticate } from "../../middlewares/user/auth";
+import { authSchema } from "../../validators/request/auth.validator";
+import { validateRequest } from "../../middlewares/request/validateRequest";
 
 const router = Router();
 
 // User Controllers Routes
-router.post("/user/register", userController.registerUser);
-router.post("/user/login", userController.loginUser);
+router.post(
+  "/user/register",
+  validateRequest(authSchema),
+  userController.registerUser,
+);
+router.post(
+  "/user/login",
+  validateRequest(authSchema),
+  userController.loginUser,
+);
 router.post("/user/resend-otp", otpController.resendOtp);
 router.post("/user/verify-otp", otpController.verifyOtp);
 router.post("/user/refresh", authController.refresh);
