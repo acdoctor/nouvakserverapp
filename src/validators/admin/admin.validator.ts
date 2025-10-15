213d24c6f1f94bc37b6d5bca3079201fbb6b781b
sachin sharma <sachinacdoctor@gmail.com>
2025-10-15 15:59:44 +0530

    fix: replace incorrect middleware logic in validator.ts with correct implementation and keep validator common for all login

--- src/validators/admin/admin.validator.ts
+++ src/validators/admin/admin.validator.ts
@@ -1,18 +1,23 @@
-import { Request, Response, NextFunction } from "express";
-import { ObjectSchema } from "joi";
+import Joi from "joi";
 
-export const validateRequest = (schema: ObjectSchema) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body, { abortEarly: false });
-
-    if (error) {
-      return res.status(400).json({
-        success: false,
-        message: "Validation failed",
-        errors: error.details.map((err) => err.message),
-      });
-    }
-
-    next();
-  };
-};
+export const authSchema = Joi.object({
+  phoneNumber: Joi.string()
+    .pattern(/^[0-9]{10}$/)
+    .required()
+    .messages({
+      "string.base": "Phone number must be 10 digits long.",
+      "string.empty": "Phone number must be 10 digits long.",
+      "string.pattern.base": "Phone number must be 10 digits long",
+      "any.required": "Phone number is required",
+    }),
+  countryCode: Joi.string()
+    .pattern(/^\+[0-9]{1,4}$/)
+    .required()
+    .messages({
+      "string.base": "Country code is required",
+      "string.empty": "Country code is required",
+      "string.pattern.base":
+        'Country code must start with a "+" followed by 1 to 4 digits',
+      "any.required": "Country code is required",
+    }),
+});
