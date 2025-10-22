6b3ef41987a501ee75c29fdd5797b7da6737a372
sachin sharma <sachinacdoctor@gmail.com>
2025-10-22 13:10:36 +0530

    feat(user): implement routes for fetching, updating, and deleting users

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -5,6 +5,7 @@ import * as authController from "../../controllers/user/auth.controller";
 import { authenticate } from "../../middlewares/user/auth";
 import { authSchema } from "../../validators/request/auth.validator";
 import { validateRequest } from "../../middlewares/request/validateRequest";
+import { updateUserSchema } from "../../validators/user/user.validator";
 
 const router = Router();
 
@@ -22,8 +23,13 @@ router.post(
 router.post("/user/resend-otp", otpController.resendOtp);
 router.post("/user/verify-otp", otpController.verifyOtp);
 router.post("/user/refresh", authController.refresh);
-router.get("/user/profile", authenticate, (req, res) => {
-  res.json({ message: "User profile accessed" });
-});
+router.get("/user/profile/:id", authenticate, userController.getUserById);
+router.get("/", userController.getAllUsers);
+router.put(
+  "/:id",
+  validateRequest(updateUserSchema),
+  userController.updateUser,
+);
+router.delete("/:id", userController.deleteUser);
 
 export default router;
