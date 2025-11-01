3c63d5edcc53825889deba474f5598cf8ff0898b
sachin sharma <sachinacdoctor@gmail.com>
2025-11-01 18:35:39 +0530

    chore: add user routes to app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import connectDB from "./config/db";
 import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/admin/admin.routes";
 import technicianRoutes from "./routes/technician/technician.routes";
+import userRoutes from "./routes/user/user.routes";
 
 const app = express();
 
@@ -12,6 +13,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 connectDB();
-app.use("/api/v1", adminRoutes, technicianRoutes);
+app.use("/api/v1", adminRoutes, technicianRoutes, userRoutes);
 
 export default app;
