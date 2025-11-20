a6c2ab1ad638b24017fde3b52612501a8abdfb5a
sachin sharma <sachinacdoctor@gmail.com>
2025-11-20 18:59:21 +0530

    refactor joi schema for tool bag validation

--- src/validators/toolsAndToolsBag/toolsAndToolsBag.validator.ts
+++ src/validators/toolsAndToolsBag/toolsAndToolsBag.validator.ts
@@ -31,26 +31,47 @@ export const removeToolIdParamsSchema = Joi.object({
 
 export const addToolBagSchema = Joi.object({
   name: Joi.string().trim().required().messages({
-    "string.empty": "Tool bag name is required",
-    "any.required": "Tool bag name is required",
+    "string.base": '"Tools bag name" should be a type of text',
+    "string.empty": '"Tools bag name" cannot be an empty field',
+    "any.required": '"Tools bag name" is a required field',
   }),
 
-  description: Joi.string().trim().allow("").optional(),
+  description: Joi.string().trim().optional().allow(null, "").messages({
+    "string.base": '"description" should be a type of text',
+  }),
 
   tools: Joi.array()
     .items(
       Joi.object({
         toolId: Joi.string().required().messages({
-          "string.empty": "toolId is required",
+          "string.base": '"toolId" should be a type of text',
+          "string.empty": '"toolId" cannot be an empty field',
+          "any.required": '"toolId" is a required field',
         }),
-        quantity: Joi.number().min(1).required().messages({
-          "any.required": "Quantity is required",
-          "number.min": "Quantity must be at least 1",
+
+        name: Joi.string().required().messages({
+          "string.base": '"Tool name" should be a type of text',
+          "string.empty": '"Tool name" cannot be an empty field',
+          "any.required": '"Tool name" is a required field',
         }),
-        name: Joi.string().required(),
-        description: Joi.string().allow("").optional(),
+
+        quantity: Joi.number().required().messages({
+          "number.base": '"quantity" should be a type of number',
+          "any.required": '"quantity" is a required field',
+        }),
+
+        description: Joi.string().optional().allow(null, "").messages({
+          "string.base": '"description" should be a type of text',
+        }),
       }),
     )
-    .optional()
-    .default([]),
+    .required()
+    .messages({
+      "array.base": '"tools" should be an array of objects',
+      "any.required": '"tools" is a required field',
+    }),
+
+  image: Joi.string().optional().default("").messages({
+    "string.base": '"image" should be a type of text',
+  }),
 });
